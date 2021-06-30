2355614d57ef9792bbb794fa4ec2461d991a1fc8
atsushinakada <nakada.atshshi@gmail.com>
2021-06-30 12:40:07 +0900

    権限変更処理の追加

--- authority_change.php
+++ authority_change.php
@@ -1,3 +1,27 @@
+<?php
+	require 'connect.php';
+	$authority = ["管理者","編集者"];
+
+	$con = new connect();
+	$pdo = $con->connectdb();
+
+	$updateResult = "";	//変更結果
+	foreach ($_POST as $key => $value) {
+		//権限をupdate
+		$stmt = $pdo->prepare('UPDATE users SET authority = :authority WHERE id = :id');
+		$stmt->bindvalue(':authority', (int)$value, PDO::PARAM_INT);
+		$stmt->bindvalue(':id', (int)$key, PDO::PARAM_INT);
+		$stmt->execute();
+
+		//権限変更したユーザーの名前を取得
+		$stmt = $pdo->prepare('SELECT name FROM users WHERE id = :id');
+		$stmt->bindvalue(':id', (int)$key, PDO::PARAM_INT);
+		$stmt->execute();
+		$username = $stmt->fetch();
+		$updateResult .= "ユーザー : ".$username['name']." ➔ 権限 : ".$authority[$value]."<br>";
+	}
+	
+?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -21,11 +45,8 @@
 			<div class="container" style="width: 70%">
 				<div style="color: #999999">
 					<?php
-						//ユーザー権限の処理を書いて～
 						print "<p class='lead'>権限が変更されたユーザー一覧：</p>";
-						foreach ($_POST as $key => $value) {
-							print "ユーザーID : ".$key." ➔ 権限 : ".$value."<br>";
-						}
+						print $updateResult;
 					?>
 				</div>
 
